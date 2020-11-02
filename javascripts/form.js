cdfd32f59a155dc5756a8a6098bfe8f613048281
Enzo Valfort <60793213+Redexes-web@users.noreply.github.com>
2020-11-02 13:59:02 +0100

    m

--- javascripts/form.js
+++ javascripts/form.js
@@ -1,1 +1,24 @@
-function addNew
+var modal = $("#myModal");
+
+// Get the button that opens the modal
+var btn = $("#addQuestion");
+
+// Get the <span> element that closes the modal
+var span = $(".close")[0];
+
+// When the user clicks the button, open the modal 
+btn.click(function() {
+  modal.css("display", "block");
+})
+
+// When the user clicks on <span> (x), close the modal
+span.click(function() {
+  modal.css("display", "none");
+})
+
+// When the user clicks anywhere outside of the modal, close it
+$(window).click(function(event) {
+  if (event.target == modal) {
+    modal.css("display", "none");
+  }
+});
